8c8765f85b993ab67e894d8f1e6cae7b15f621dc
dalibor.klima <dalibor.klima@VT1-04>
2023-03-06 10:42:04 +0100

    Přidejte soubory projektu.

---
+++ já/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine(@"Ahoj, jsem Dalibor Klíma, chodím na SŠ Educhem a mám rád/baví mě: ");
+
+string[] zajmy = { "anime", "hry", "spánek", "filmy/seriály" };
+foreach (var item in zajmy)
+{
+    Console.WriteLine("- " + item);
+}
